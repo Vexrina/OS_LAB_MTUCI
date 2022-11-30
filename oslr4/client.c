a9bcfb2169d30f1c3eed3f5dba5463bbdb658637
Wi1d10neW01f <gameofsteam.ru@gmail.com>
2022-11-30 13:01:14 +0300

    added channels

---
+++ oslr4/client.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(){
+   int fd = open("myfifo",O_WRONLY);
+   if(fd<0){
+        perror("open");
+        return 1;
+   }
+   char buf[1024];
+   while (1)
+   {
+        printf("please Enter# ");
+        fflush(stdout);
+
+        ssize_t len = read(0,buf,sizeof(buf)-1);
+        if (len>0)
+        {
+            buf[len]=0;
+            write(fd,buf,strlen(buf));
+        }
+        else{
+            perror("read");
+            return 1;
+        }
+   }
+   close(fd);
+   return 0;
+}
---
+++ oslr4/server.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+    umask(0);
+    int ret = mkfifo("myfifo",0666);
+    if(ret<0){
+        perror("mkfifo");
+        return 1;
+    }
+    
+    char buf[1024];
+    int fd = open("myfifo",O_RDONLY);
+    while (1)
+    {
+        int len = read(fd,buf,sizeof(buf)-1);
+        if(len>0){
+            buf[len] = 0;
+            printf("client say:%s", buf);
+        }
+        else if (len==0){
+            printf("client quit!\n");
+            break;
+        }
+        else{
+            perror("read");
+            return 1;
+        }
+    }
+    close(fd);
+    return 0;
+}
