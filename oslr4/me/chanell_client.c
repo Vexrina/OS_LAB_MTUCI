33ae213d0586f924a2ab02ac18c7e60d29d4fcf1
Wi1d10neW01f <gameofsteam.ru@gmail.com>
2022-12-10 18:36:48 +0300

    completed channels

---
+++ oslr4/me/chanell_client.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(){
+   int fd = open("myfifo",O_WRONLY);
+   if(fd<0){
+        perror("open");
+        return 1;
+   }
+   char buf[1024];
+   while (1)
+   {
+        printf("please Enter mtx and order#");
+        fflush(stdout);
+        ssize_t len = read(0,buf,sizeof(buf)-1);
+        if (len>0)
+        {
+            buf[len]=0;
+	    write(fd,buf,strlen(buf));
+        }
+        else{
+            perror("read");
+            return 1;
+        }
+   }
+   close(fd);
+   return 0;
+}
---
+++ oslr4/me/chanell_server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+int main()
+{
+    umask(0);
+    int ret = mkfifo("myfifo", 0666);
+    if (ret < 0)
+    {
+        perror("mkfifo");
+        return 1;
+    }
+    char buf[1024];
+    int fd = open("myfifo", O_RDONLY);
+    while (1)
+    {
+        int len = read(fd, buf, sizeof(buf) - 1);
+        if (len > 0)
+        {
+            char command[] = "python3 ./gauss.py ";
+            buf[len] = 0;
+            strcat(command, buf);
+            // printf(command);
+            system(command);
+        }
+        else if (len == 0)
+        {
+            printf("client quit!\n");
+            break;
+        }
+        else
+        {
+            perror("read");
+            return 1;
+        }
+    }
+    close(fd);
+    return 0;
+}
